80314add76f9937455803392e1ddd3f0903bf45f
krasenDimitrov90 <krasendimitrov90@gmail.com>
2022-11-22 20:04:20 +0200

    implementing fetch api

--- weather-app-react/src/App.js
+++ weather-app-react/src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
+import { useState } from 'react';
 
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 
 function App() {
+
+
     return (
         <>
-            <Header />
-            <Main />
+                <Header />
+                <Main />
         </>
     );
 }
--- weather-app-react/src/components/Header/Header.js
+++ weather-app-react/src/components/Header/Header.js
@@ -1,15 +1,42 @@
 import './Header.style.scss';
+import { useState, useEfect } from 'react';
 
-const Header = () => (
-    <header className='header'>
-        <section className='header-left'>
-            <a href="#">Home</a>
-        </section>
-        <section className='header-right'>
-            <input type="text" placeholder='Write city here'/>
-            <i class="fa-sharp fa-solid fa-magnifying-glass"></i>
-        </section>
-    </header>
-);
+import { getTown, getOneDayWeatherInfo, getFiveDayWeatherInfo } from '../../servicies/requests';
+
+
+
+const Header = () => {
+
+    const [city, setCity] = useState('');
+    const [cityKey, setCityKey] = useState('');
+    const [countryId, setCountryId] = useState('');
+    const [weather, setWeather] = useState('');
+
+    const handleClick = async (e) => {
+        e.preventDefault();
+        const { city } = Object.fromEntries(new FormData(e.target))
+        const data = await getTown(city)
+        const townData = data[0]
+
+        const townKey = townData.Key;
+        const id = townData.Country.ID;
+
+        const cityWeather = await getFiveDayWeatherInfo(townKey);
+
+        console.log( 'in handleClick', cityWeather);
+    }
+
+    return (
+        <header className='header'>
+            <section className='header-left'>
+                <a href="#">Home</a>
+            </section>
+            <form onSubmit={handleClick} className='header-right'>
+                <input type="text" name='city' placeholder='Write city here' />
+                <button type='submit'><i class="fa-sharp fa-solid fa-magnifying-glass" ></i></button>
+            </form>
+        </header>
+    )
+};
 
 export default Header;
